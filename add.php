4673acddb5c504381dd988b7b0f389d3becfd087
星禾 <954204979@qq.com>
2023-11-22 20:38:32 +0800

    优化代码,新增重复url判断

--- add.php
+++ add.php
@@ -5,7 +5,7 @@ include 'config.php';
 
 function generateRandomString($length = 6)
 {
-    $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
     $randomString = '';
 
     for ($i = 0; $i < $length; $i++) {
@@ -19,7 +19,7 @@ if (isset($_POST['url'])) {
     if (strpos($_POST['url'], 'http') !== false) {
         echo '';
     } else {
-        die('链接必须使用 http://或https:// 开头');
+        die('链接必须使用 http:// 或 https:// 开头');
     }
 
     $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
@@ -32,36 +32,41 @@ if (isset($_POST['url'])) {
 
     $t = addcslashes(mysqli_real_escape_string($conn, base64_encode($_POST['url'])), "%_");
     $ip_t = $_SERVER["REMOTE_ADDR"];
-    $short = generateRandomString(); // 生成随机字符串
 
-    $sql = "select num from go_to_url where url='$t'";
+    $short = generateRandomString(); // 初始生成随机字符串
 
-    mysqli_select_db($conn, $dbname);
+    // 循环直到找到不重复的 $short
+    while (true) {
+        $sql = "SELECT COUNT(*) FROM go_to_url WHERE short_url = '$short'";
+        $checkResult = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_array($checkResult);
+
+        if ($row[0] == 0) {
+            // 生成的 $short 不重复，跳出循环
+            break;
+        } else {
+            // 生成的 $short 已存在，重新生成
+            $short = generateRandomString();
+        }
+    }
+
+    $sql = "INSERT INTO go_to_url " .
+        "(url,short_url,ip,add_date,uid) " .
+        "VALUES " .
+        "('$t','$short','$ip_t',NOW(),0)";
+
     $retval = mysqli_query($conn, $sql);
 
-    if ($retval->num_rows == 0) {
-        $sql = "INSERT INTO go_to_url " .
-            "(url,short_url,ip,add_date,uid) " .
-            "VALUES " .
-            "('$t','$short','$ip_t',NOW(),0)";
+    if (!$retval) {
+        die('无法插入数据: ' . mysqli_error($conn));
+    }
 
-        $retval = mysqli_query($conn, $sql);
+    $sql = 'SELECT MAX(num) FROM go_to_url';
 
-        if (!$retval) {
-            die('无法插入数据: ' . mysqli_error($conn));
-        }
+    $retval = mysqli_query($conn, $sql);
 
-        $sql = 'SELECT MAX(num) FROM go_to_url';
-
-        $retval = mysqli_query($conn, $sql);
-
-        while ($row = mysqli_fetch_array($retval)) {
-            echo $my_url . $short;
-        }
-    } else {
-        while ($row = mysqli_fetch_array($retval)) {
-            echo $my_url . $short;
-        }
+    while ($row = mysqli_fetch_array($retval)) {
+        echo $my_url . $short;
     }
 
     mysqli_close($conn);
